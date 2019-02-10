6889ae140d2bd7e485dc89c1ecabd89edccac1a2
Sean Haneberg <haneberg@gmail.com>
2019-02-09 19:28:50 -0800

    Animate with react-spring (#8)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import BlockSVGRenderer from './BlockRendererSVG';
+import { Spring } from 'react-spring/renderprops';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +8,42 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          Harmony Blocks
         </header>
         <div className="svg-container" style={{ border: "black", margin: "10px" }}>
           <svg x={10} y={10} height={400} width={500}>
-            <BlockSVGRenderer
-              x={10}
-              y={10}
-              width={100}
-              height={10}
-              red={1.0}
-              green={0}
-              blue={0}
-              indicatorXOffset={10}
-              indicatorYOffset={10}
-              indicatorHeight={10}
-            />
+            <Spring
+              to={{
+                red: 1.0,
+                green: 0.2,
+                blue: 0.3,
+                indicatorXOffset: 100,
+              }}
+              from={{
+                red: 0.8,
+                green: 0.8,
+                blue: 0.8,
+                indicatorXOffset: 0,
+              }}
+              config={{
+                duration: 250,
+                reset: true,
+              }}
+            >
+              {(animatedProperties) => {
+                const props = {
+                  x: 10,
+                  y: 10,
+                  width: 100,
+                  height: 20,
+                  indicatorYOffset: 10,
+                  indicatorHeight: 20,
+                  ...animatedProperties,
+                };
+
+                return <BlockSVGRenderer {...props} />;
+              }}
+            </Spring>
           </svg>
         </div>
       </div>
