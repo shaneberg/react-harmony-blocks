28a3852c842b42aa2f937cc93008db7a2bb42071
Sean Haneberg <haneberg@gmail.com>
2019-02-10 23:40:38 -0800

    Figure out how to do looping (#10)

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,31 @@ import { Spring } from 'react-spring/renderprops';
 import './App.css';
 
 class App extends Component {
+
+  onRest() {
+    this.setState({reset: !(this.state && this.state.reset)});
+  }
+
+
   render() {
+    const init = this.state && this.state.reset;
+    const to = {
+      red: init ? 0.8 : 1.0,
+      green: init ? 0.8 : 0.2,
+      blue: init ? 0.8 : 0.3,
+      indicatorXOffset: init ? 0 : 100,
+    };
+
     return (
-      <div className="App">
+      <div className="App" >
         <header className="App-header">
           Harmony Blocks
         </header>
         <div className="svg-container" style={{ border: "black", margin: "10px" }}>
           <svg x={10} y={10} height={400} width={500}>
             <Spring
-              to={{
-                red: 1.0,
-                green: 0.2,
-                blue: 0.3,
-                indicatorXOffset: 100,
-              }}
+              immediate={init}
+              to={to}
               from={{
                 red: 0.8,
                 green: 0.8,
@@ -26,9 +36,10 @@ class App extends Component {
                 indicatorXOffset: 0,
               }}
               config={{
-                duration: 250,
+                duration: 500,
                 reset: true,
               }}
+              onRest={this.onRest.bind(this)}
             >
               {(animatedProperties) => {
                 const props = {
