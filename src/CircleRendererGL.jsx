154229da149e9807ed3fac3448fb7d6f58f5cfb8
Sean Haneberg <haneberg@gmail.com>
2019-02-21 19:42:13 -0800

    draw basic circle

--- src/CircleRendererGL.jsx
+++ src/CircleRendererGL.jsx
@@ -5,12 +5,26 @@ import { Surface } from 'gl-react-dom';
 const shaders = Shaders.create({
   helloBlue: {
     frag: GLSL`
-precision highp float;
+precision mediump float;
 varying vec2 uv;
-uniform float blue;
+uniform vec2 u_resolution;
+
+float circle(in vec2 _st, in float _radius){
+  vec2 dist = _st-vec2(0.5);
+  return 1.-smoothstep(_radius-(_radius*0.01),
+                       _radius+(_radius*0.01),
+                       dot(dist,dist)*4.0);
+}
+
 void main() {
-  gl_FragColor = vec4(uv.x, uv.y, blue, 1.0);
-}`
+  vec2 st = gl_FragCoord.xy/u_resolution.xy;
+
+	vec3 color = vec3(circle(st,0.3));
+
+	gl_FragColor = vec4( color, 1.0 );
+}
+`
+
   }
 });
 
@@ -18,8 +32,8 @@ class CircleRendererGL extends Component {
 
   render() {
     return (
-      <Surface width={300} height={150}>
-        <Node shader={shaders.helloBlue} uniforms={{ blue: 0.5 }} />;
+      <Surface width={200} height={100}>
+        <Node shader={shaders.helloBlue} uniforms={{ u_resolution: [200, 200] }}/>
       </Surface>
     );
   }
