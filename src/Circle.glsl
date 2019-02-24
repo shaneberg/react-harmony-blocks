67877cf7fc9a5b942a863c5de1695e219b19b652
Sean Haneberg <haneberg@gmail.com>
2019-02-23 18:53:16 -0800

    add default color

--- src/Circle.glsl
+++ src/Circle.glsl
@@ -10,14 +10,15 @@ uniform float u_time;
 #define PI 3.141592653589793
 #define TWO_PI 6.283185307179586
 
-float circle(in vec2 st, in float radius){
+vec3 circle(in vec2 st, in float radius){
+  vec3 color = vec3(.2, .2, .9);
   vec2 center = vec2(.5, .5);
   float dist = distance(st, center);
-  return 1. - step(radius, dist);
+  return color - step(radius, dist);
 }
 
 void main(){
   vec2 st = gl_FragCoord.xy / u_resolution.xy;
-  vec3 color = vec3(circle(st,.1));
-  gl_FragColor = vec4(color,0.9);
+  vec3 color = circle(st,.1);
+  gl_FragColor = vec4(color,1.0);
 }
