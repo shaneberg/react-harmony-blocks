a118f1e2143febf14334dec7d75db71700a80876
Sean Haneberg <haneberg@gmail.com>
2019-02-23 18:47:16 -0800

    simplify circle

--- src/Circle.glsl
+++ src/Circle.glsl
@@ -10,15 +10,14 @@ uniform float u_time;
 #define PI 3.141592653589793
 #define TWO_PI 6.283185307179586
 
-float circle(in vec2 _st,in float _radius){
-  vec2 dist=_st-vec2(.5);
-  return 1.-smoothstep(_radius-(_radius*.01),
-  _radius+(_radius*.01),
-  dot(dist,dist)*4.);
+float circle(in vec2 st, in float radius){
+  vec2 center = vec2(.5, .5);
+  float dist = distance(st, center); 
+  return 1. - step(radius, dist);
 }
 
 void main(){
-  vec2 st=gl_FragCoord.xy/u_resolution.xy;
-  vec3 color=vec3(circle(st,.3));
-  gl_FragColor=vec4(color,1.);
+  vec2 st = gl_FragCoord.xy / u_resolution.xy;
+  vec3 color = vec3(circle(st,.1));
+  gl_FragColor = vec4(color,0.9);
 }
