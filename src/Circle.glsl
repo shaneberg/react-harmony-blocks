9f92f13a6ccdadb45ab9d936a682d492a0a836ce
Sean Haneberg <haneberg@gmail.com>
2019-02-23 18:47:21 -0800

    simplify circle

--- src/Circle.glsl
+++ src/Circle.glsl
@@ -12,7 +12,7 @@ uniform float u_time;
 
 float circle(in vec2 st, in float radius){
   vec2 center = vec2(.5, .5);
-  float dist = distance(st, center); 
+  float dist = distance(st, center);
   return 1. - step(radius, dist);
 }
 
