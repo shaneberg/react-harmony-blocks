66958f653bab4ab4ea3f6b10381bf48db4bd173b
Sean Haneberg <haneberg@gmail.com>
2019-02-28 22:05:10 -0800

    add coef

--- src/DotContainer.jsx
+++ src/DotContainer.jsx
@@ -6,15 +6,17 @@ class DotContainer extends Component {
 
   render() {
     const blocks = this.props.blocks.map((block, index) => {
-      const progress = block.progress % 1.0;
       let dots = [];
       // TODO: Figure out how to do this for fractional ratios
       for (let i = 0; i < block.hz; i++) {
+        const coef = (i + 1);
+        const progress = 1 - ((coef) * block.progress % 1.0)
+
         dots.push(
           <DotRendererSVG
             key={index + i}
             x={15 * (index + 5)}
-            y={20 * i}
+            y={10 + (20 * i)}
             width={5}
             height={5}
             red={Utils.lerp(block.fromRed, block.toRed, progress)}
